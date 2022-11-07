b7bf66f0237960823002ac4661f7ae144be2ea04
André Sousa <diogo.saraiva94@gmail.com>
2022-11-07 11:23:56 +0000

    Portfolio v2.0
    Brand new website.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,33 @@
-// SCROLL REVEAL
+const navButton = document.querySelector(".navbar__nav-toggle");
+const menu = document.querySelector(".navbar__mobile-nav");
+const closeButton = document.querySelector(".navbar__nav-toggle__close");
 
-// HOME
-ScrollReveal().reveal('#navigation', { delay: 250 });
-ScrollReveal().reveal('#home-showcase', { delay: 500, distance: '30%', duration: 2000 });
-ScrollReveal().reveal('#about', { delay: 100, distance: '30%', duration: 1500 });
-ScrollReveal().reveal('#contacts', { delay: 100, distance: '30%', duration: 1500 });
-ScrollReveal().reveal('#footer', { delay: 100, distance: '30%', duration: 1500 });
-ScrollReveal().reveal('.video-container', { delay: 100, distance: '30%', duration: 1500 });
-
-// CATEGORIES
-ScrollReveal().reveal('#category-header', { delay: 100, distance: '30%', duration: 1500 });
-ScrollReveal().reveal('.showcase-image', { delay: 200, distance: '30%', duration: 1500 });
-ScrollReveal().reveal('#bottom-nav', { delay: 200, distance: '30%', duration: 1500 });
-
-
-
-// MENU
-const navToggle = document.getElementById('nav-toggle');
-const navBody = document.querySelector('.mobile-nav-body');
-
+// INITIAL STATE
 let showMenu = false;
 
-navToggle.addEventListener('click', toggleMenu);
+// EVENT LISTENER
+navButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
-        navToggle.classList.add('close');
-        navBody.classList.add('show');
-        document.body.style.overflow = 'hidden';
-        showMenu = true;
-    } else {
-        navToggle.classList.remove('close');
-        navBody.classList.remove('show');
-        document.body.style.overflow = 'visible';
-        showMenu = false;
-    }
+  if (!showMenu) {
+    menu.classList.add("show");
+
+    showMenu = true;
+  } else {
+    menu.classList.remove("show");
+    showMenu = false;
+  }
 }
 
+closeButton.addEventListener("click", hideMenu);
 
+function hideMenu() {
+  if (!showMenu) {
+    menu.classList.add("show");
 
-// SCROLL
-const scrollup = document.getElementById('scrollup');
-
-window.addEventListener('scroll', () => {
-    let y = window.scrollY;
-
-    if(y > 800) {
-        scrollup.style.display ='block';
-    } else {
-        scrollup.style.display ='none';
-    }
-});
-
+    showMenu = true;
+  } else {
+    menu.classList.remove("show");
+    showMenu = false;
+  }
+}
